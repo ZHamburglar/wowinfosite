0c463ef285feff89cf4d25951151e3d06f12b65b
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-01 14:22:12 -0500

    working on mounts json

--- backend/app.js
+++ backend/app.js
@@ -4,6 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var fs = require('fs')
+
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -68,5 +70,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+
 
 module.exports = app;
--- backend/routes/wowmount.js
+++ backend/routes/wowmount.js
@@ -3,6 +3,7 @@ var router = express.Router();
 var request = require('request');
 var path = require('path');
 var qs = require('querystring');
+var fs = require('fs')
 require('dotenv').config()
 
 
@@ -11,11 +12,28 @@ require('dotenv').config()
 router.get('/', function(req, res, next) {
   request('https://us.api.battle.net/wow/mount/?locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
     function (error, response, body) {
+      fs.readFile('./json/weekly/mounts.json', 'utf-8', function(err, data) {
+        if (err) throw err
+        var arrayOfObjects = JSON.parse(data)
+        arrayOfObjects.lastUpdated = [];
+        arrayOfObjects.lastUpdated.push(Date.now())
+
+        arrayOfObjects.mounts= [];
+        arrayOfObjects.mounts.push(body);
+        console.log('arrayOfObjects ',arrayOfObjects)
+        fs.writeFile('./json/weekly/mounts.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+          if (err) throw err
+          console.log('Done!')
+        })
+        // User JSON.parse later on to targer the data.
+      })
       console.log('error:', error);
       console.log('statusCode:', response && response.statusCode);
       console.log('body:', body);
       res.send(body);
   });
 });
+
+
 
 module.exports = router;
