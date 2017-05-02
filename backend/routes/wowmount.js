b80fc236813548324c3ae677473a7f79696bdd53
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-02 11:52:00 -0500

    now the original call will not be empty

--- backend/routes/wowmount.js
+++ backend/routes/wowmount.js
@@ -11,44 +11,42 @@ router.get('/', function(req, res, next) {
 
   //testing the if statement
   fs.readFile('./json/weekly/mounts.json', 'utf-8', function(err, data) {
+    if (err) throw err
     var arrayOfObjects = JSON.parse(data)
     lastJSONUpdate = arrayOfObjects.lastUpdated
     console.log('this section is working.', lastJSONUpdate)
     if ((Date.now()-lastJSONUpdate)>700000000){
-      console.log('wow that is a lot')
+      console.log('Week has passed...calling API')
+      request('https://us.api.battle.net/wow/mount/?locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
+        function (error, response, body) {
+          fs.readFile('./json/weekly/mounts.json', 'utf-8', function(err, data) {
+            var arrayOfObjects = JSON.parse(data)
+            arrayOfObjects.lastUpdated = [];
+            arrayOfObjects.lastUpdated.push(Date.now())
+            jsonconvert = JSON.parse(body)
+            arrayOfObjects.mounts= [];
+            arrayOfObjects.mounts.push(jsonconvert);
+            console.log('arrayOfObjects ',arrayOfObjects)
+            fs.writeFile('./json/weekly/mounts.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+              if (err) throw err
+              console.log('JSON Sent!');
+              sendJSON();
+            })
+          })
+          console.log('error:', error);
+          console.log('statusCode:', response && response.statusCode);
+      });
     } else {
-      console.log('not quite there yet')
+      sendJSON();
     }
   });
 
-
-  request('https://us.api.battle.net/wow/mount/?locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
-    function (error, response, body) {
-      fs.readFile('./json/weekly/mounts.json', 'utf-8', function(err, data) {
-        if (err) throw err
-        var arrayOfObjects = JSON.parse(data)
-        arrayOfObjects.lastUpdated = [];
-        arrayOfObjects.lastUpdated.push(Date.now())
-        jsonconvert = JSON.parse(body)
-        arrayOfObjects.mounts= [];
-        arrayOfObjects.mounts.push(jsonconvert);
-        console.log('arrayOfObjects ',arrayOfObjects)
-        fs.writeFile('./json/weekly/mounts.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
-          if (err) throw err
-          console.log('Done!')
-        })
-      })
-      console.log('error:', error);
-      console.log('statusCode:', response && response.statusCode);
-      // console.log('body:', body);
-
-      var filePath = './json/weekly/mounts.json'
-      var resolvedPath = path.resolve(filePath);
-      console.log(resolvedPath);
-      return res.sendFile(resolvedPath);
-  });
+  function sendJSON(){
+    var filePath = './json/weekly/mounts.json'
+    var resolvedPath = path.resolve(filePath);
+    console.log("JSON Being sent: ",resolvedPath);
+    return res.sendFile(resolvedPath);
+  }
 });
-
-
 
 module.exports = router;
