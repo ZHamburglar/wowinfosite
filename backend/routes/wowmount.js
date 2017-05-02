69f3f6c9c01274a72442159619217bb6b4664bd9
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-02 11:15:56 -0500

    working on loading from json rather than API call

--- backend/routes/wowmount.js
+++ backend/routes/wowmount.js
@@ -5,8 +5,23 @@ var path = require('path');
 var qs = require('querystring');
 var fs = require('fs')
 require('dotenv').config()
-
+// 1 week = 700000000
 router.get('/', function(req, res, next) {
+  console.log("current time: ", Date.now())
+
+  //testing the if statement
+  fs.readFile('./json/weekly/mounts.json', 'utf-8', function(err, data) {
+    var arrayOfObjects = JSON.parse(data)
+    lastJSONUpdate = arrayOfObjects.lastUpdated
+    console.log('this section is working.', lastJSONUpdate)
+    if ((Date.now()-lastJSONUpdate)>700000000){
+      console.log('wow that is a lot')
+    } else {
+      console.log('not quite there yet')
+    }
+  });
+
+
   request('https://us.api.battle.net/wow/mount/?locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
     function (error, response, body) {
       fs.readFile('./json/weekly/mounts.json', 'utf-8', function(err, data) {
@@ -25,7 +40,7 @@ router.get('/', function(req, res, next) {
       })
       console.log('error:', error);
       console.log('statusCode:', response && response.statusCode);
-      console.log('body:', body);
+      // console.log('body:', body);
 
       var filePath = './json/weekly/mounts.json'
       var resolvedPath = path.resolve(filePath);
