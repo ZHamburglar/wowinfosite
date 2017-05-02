642bf4496c8a242b4830e20eeba6d5561f25a8fa
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-02 13:03:49 -0500

    clearing script works

--- backend/clearjson.js
+++ backend/clearjson.js
@@ -1,38 +1,28 @@
 const fs = require('fs');
 var gutil = require('gulp-util');
 
+buildclearjson('mounts', 'weekly');
+buildclearjson('characterclass', 'weekly')
+buildclearjson('characterraces', 'weekly')
 
-fs.stat('./json/weekly/testing.json', function (err, stats) {
-   console.log(stats);
-   if (err) {
-       return console.error(err);
-   }
-   fs.unlink('./json/weekly/testing.json',function(err){
-        if(err) return console.log(err);
-        console.log(gutil.colors.green('File exists. Deleting now ...'));
-   });
-});
 
-fs.stat('./json/weekly/testing2.json', function (err, stats) {
-   console.log(stats);
-   if (err) {
-       return console.error(err);
-   }
-   fs.unlink('./json/weekly/testing2.json',function(err){
-        if(err) return console.log(gutil.colors.red(err));
-        console.log('file deleted successfully');
-   });
-});
+function buildclearjson(extension, timeline){
+  fs.stat('./json/'+timeline+'/'+extension+'.json', function (err, stats) {
+    //  console.log(stats);
+     if (err) {
+         return console.log(gutil.colors.magenta(err));
+     }
+     fs.unlink('./json/'+timeline+'/'+extension+'.json',function(err){
+          if(err) return console.log(err);
+          console.log(gutil.colors.green('File exists. Deleting now ...'));
+     });
+     fs.writeFile('./json/'+timeline+'/'+extension+'.json', '{"lastUpdated":[],"'+extension+'":[]}', function (err) {
+          if (err) throw err;
+          console.log(''+extension+'.json has been created.');
+      });
+  });
+}
 
-fs.stat('./json/weekly/testing3.json', function (err, stats) {
-   console.log(stats);
-   if (err) {
-       return console.error(err);
-   }
-   fs.unlink('./json/weekly/testing3.json',function(err){
-        if(err) return console.log(err);
-        console.log('file deleted successfully');
-   });
-});
+
 
 console.log('Clear JSON Script is working.')
