5f9e9899ea12ddd4ccd752e482e2b1d9f646f0f9
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-02 16:35:47 -0500

    working on checking/creating character jsons

--- backend/routes/wowcharacter.js
+++ backend/routes/wowcharacter.js
@@ -3,30 +3,62 @@ var router = express.Router();
 var request = require('request');
 var path = require('path');
 var qs = require('querystring');
-require('dotenv').config()
+require('dotenv').config();
+var fs = require('fs');
 
-
-
-
-router.get('/', function(req, res, next) {
-  request('https://us.api.battle.net/wow/character/malganis/FinnyFerret?fields=audit,titles,talents,stats,statistics,reputation,quests,pvp,progression,professions,petSlots,mounts,pets,feed,items,achievements,appearance,guild,hunterPets&locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
-    function (error, response, body) {
-      console.log('error:', error);
-      console.log('statusCode:', response && response.statusCode);
-      console.log('body:', body);
-      res.send(body);
-
-  });
-});
 
 router.get('/:server/:charactername', function(req, res, next) {
-  request('https://us.api.battle.net/wow/character/'+req.params.server+'/'+req.params.charactername+'?fields=audit,titles,talents,stats,statistics,reputation,quests,pvp,progression,professions,petSlots,mounts,pets,feed,items,achievements,appearance,guild,hunterPets&locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
-    function (error, response, body) {
-      console.log('error:', error);
-      console.log('statusCode:', response && response.statusCode);
-      console.log('body:', body);
-      res.send(body);
-  });
+    console.log("current time: ", Date.now())
+    var server = req.params.server
+    var character = req.params.charactername
+    //testing the if statement
+
+
+
+    // Check if the character json exists, then if it doesn't then create a new one;
+
+
+
+
+
+
+    fs.readFile('./json/characters/'+server+'/'+character+'.json', 'utf-8', function(err, data) {
+      if (err) throw err
+      var arrayOfObjects = JSON.parse(data)
+      lastJSONUpdate = arrayOfObjects.lastUpdated
+      console.log('this section is working.', lastJSONUpdate)
+      if ((Date.now()-lastJSONUpdate)>700000000){
+        console.log('Week has passed...calling API')
+        request('https://us.api.battle.net/wow/character/'+req.params.server+'/'+req.params.charactername+'?fields=audit,titles,talents,stats,statistics,reputation,quests,pvp,progression,professions,petSlots,mounts,pets,feed,items,achievements,appearance,guild,hunterPets&locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
+          function (error, response, body) {
+            fs.readFile('./json/weekly/mounts.json', 'utf-8', function(err, data) {
+              var arrayOfObjects = JSON.parse(data)
+              arrayOfObjects.lastUpdated = [];
+              arrayOfObjects.lastUpdated.push(Date.now())
+              jsonconvert = JSON.parse(body)
+              arrayOfObjects.mounts= [];
+              arrayOfObjects.mounts.push(jsonconvert);
+              console.log('arrayOfObjects ',arrayOfObjects)
+              fs.writeFile('./json/characters/'+server+'/'+character+'.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+                if (err) throw err
+                console.log('JSON Sent!');
+                sendJSON();
+              })
+            })
+            console.log('error:', error);
+            console.log('statusCode:', response && response.statusCode);
+        });
+      } else {
+        sendJSON();
+      }
+    });
+
+    function sendJSON(){
+      var filePath = './json/characters/'+server+'/'+character+'.json'
+      var resolvedPath = path.resolve(filePath);
+      console.log("JSON Being sent: ",resolvedPath);
+      return res.sendFile(resolvedPath);
+    }
 });
 
 
--- frontend/main.js
+++ frontend/main.js
@@ -1,21 +1,43 @@
-$( document ).ready(function() {
-    $('button').on('click', buttonclick)
+$(document).ready(function() {
+  $('button#testbutton').on('click', buttonclick);
+  $('button#characterbutton').on('click', characterbuttonclick);
 });
 
-function buttonclick(){
+function buttonclick() {
   {
-  $.ajax({
-    url: 'http://localhost:3000/wowmount'
-  })
-  .done(function(response){
-    console.log('data: ', response);
-  })
-  .fail(function(jqXHR, textStatus, errorThrown) {
-    alert('oh no!');
-    console.log("Bad Request! ", textStatus, errorThrown);
-  })
-  .always(function() {
-    console.log('I always run no matter what');
-  })
-}
+    $.ajax({
+        url: 'http://localhost:3000/wowmount'
+      })
+      .done(function(response) {
+        console.log('data: ', response);
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        alert('oh no!');
+        console.log("Bad Request! ", textStatus, errorThrown);
+      })
+      .always(function() {
+        console.log('I always run no matter what');
+      })
+  }
+};
+
+function characterbuttonclick() {
+  var servername = $('#server').val();
+  var charactername = $('#charactername').val();
+  console.log("http://localhost:3000/wowcharacter/"+servername+"/"+charactername)
+  {
+    $.ajax({
+        url: "http://localhost:3000/wowcharacter/"+servername+"/"+charactername
+      })
+      .done(function(response) {
+        console.log('data: ', response);
+      })
+      .fail(function(jqXHR, textStatus, errorThrown) {
+        alert('oh no!');
+        console.log("Bad Request! ", textStatus, errorThrown);
+      })
+      .always(function() {
+        console.log('I always run no matter what');
+      })
+  }
 }
