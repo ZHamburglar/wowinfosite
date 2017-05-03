ea5a33891e87162041cfa517166698bbaaeadabe
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-02 21:41:29 -0500

    working on building character jsons

--- backend/routes/wowcharacter.js
+++ backend/routes/wowcharacter.js
@@ -11,18 +11,39 @@ router.get('/:server/:charactername', function(req, res, next) {
     console.log("current time: ", Date.now())
     var server = req.params.server
     var character = req.params.charactername
-    //testing the if statement
+    var firstLetter = character.charAt(0).toUpperCase()
+    console.log("First Letter ",firstLetter)
+    if (fs.exists('./json/characters/'+server)){
+      console.log('1')
+    } else {
+      console.log('2')
+      fs.mkdir('./json/characters/'+server);
+    }
+
+    if (fs.exists('./json/characters/'+server+'/'+firstLetter)){
+      console.log('3')
+    } else {
+      console.log('4')
+      fs.mkdir('./json/characters/'+server+'/'+firstLetter);
+    }
+
+    if (fs.exists('./json/characters/'+server+'/'+firstLetter+'/'+character+'.json')){
+      console.log('5')
+    } else {
+      console.log('6')
+      // fs.writeFile('./json/characters/'+server+'/'+firstLetter+'/'+character+'.json', '{"lastUpdated":[],"character":[]}');
+      fs.writeFile('./json/characters/'+server+'/'+firstLetter+'/'+character+'.json', '{"lastUpdated":[],"character":[]}', function (err) {
+           if (err) throw err;
+           console.log('.json has been created.');
+       });
+    }
 
 
 
-    // Check if the character json exists, then if it doesn't then create a new one;
 
 
 
-
-
-
-    fs.readFile('./json/characters/'+server+'/'+character+'.json', 'utf-8', function(err, data) {
+    fs.readFile('./json/characters/'+server+'/'+firstLetter+'/'+character+'.json', 'utf-8', function(err, data) {
       if (err) throw err
       var arrayOfObjects = JSON.parse(data)
       lastJSONUpdate = arrayOfObjects.lastUpdated
@@ -31,15 +52,15 @@ router.get('/:server/:charactername', function(req, res, next) {
         console.log('Week has passed...calling API')
         request('https://us.api.battle.net/wow/character/'+req.params.server+'/'+req.params.charactername+'?fields=audit,titles,talents,stats,statistics,reputation,quests,pvp,progression,professions,petSlots,mounts,pets,feed,items,achievements,appearance,guild,hunterPets&locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
           function (error, response, body) {
-            fs.readFile('./json/weekly/mounts.json', 'utf-8', function(err, data) {
+            fs.readFile('./json/characters/'+server+'/'+firstLetter+'/'+character+'.json', 'utf-8', function(err, data) {
               var arrayOfObjects = JSON.parse(data)
               arrayOfObjects.lastUpdated = [];
               arrayOfObjects.lastUpdated.push(Date.now())
               jsonconvert = JSON.parse(body)
-              arrayOfObjects.mounts= [];
-              arrayOfObjects.mounts.push(jsonconvert);
+              arrayOfObjects.character= [];
+              arrayOfObjects.character.push(jsonconvert);
               console.log('arrayOfObjects ',arrayOfObjects)
-              fs.writeFile('./json/characters/'+server+'/'+character+'.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+              fs.writeFile('./json/characters/'+server+'/'+firstLetter+'/'+character+'.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
                 if (err) throw err
                 console.log('JSON Sent!');
                 sendJSON();
@@ -54,7 +75,7 @@ router.get('/:server/:charactername', function(req, res, next) {
     });
 
     function sendJSON(){
-      var filePath = './json/characters/'+server+'/'+character+'.json'
+      var filePath = './json/characters/'+server+'/'+firstLetter+'/'+character+'.json'
       var resolvedPath = path.resolve(filePath);
       console.log("JSON Being sent: ",resolvedPath);
       return res.sendFile(resolvedPath);
--- backend/routes/wowmount.js
+++ backend/routes/wowmount.js
@@ -6,15 +6,21 @@ var qs = require('querystring');
 var fs = require('fs')
 require('dotenv').config()
 // 1 week = 700000000
+
 router.get('/', function(req, res, next) {
+
   console.log("current time: ", Date.now())
 
-  //testing the if statement
+  //reads the mounts.json file
   fs.readFile('./json/weekly/mounts.json', 'utf-8', function(err, data) {
+    // if it doesn't exist, throws an error
     if (err) throw err
+    // Parses the JSON data that is in the mounts.json in an object that can be used later
     var arrayOfObjects = JSON.parse(data)
+    // Defines lastJSONUpdate as a result of arrayOfObjects.lastUpdated
     lastJSONUpdate = arrayOfObjects.lastUpdated
     console.log('this section is working.', lastJSONUpdate)
+    // if  the current time is more than 700000000 milliseconds after the time when it was last update, then run the following function.
     if ((Date.now()-lastJSONUpdate)>700000000){
       console.log('Week has passed...calling API')
       request('https://us.api.battle.net/wow/mount/?locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
