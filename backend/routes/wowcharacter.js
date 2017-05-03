e0ef2682207182891b9e07da2d51cb70fcb24819
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-03 17:15:03 -0500

    removed the first readfile

--- backend/routes/wowcharacter.js
+++ backend/routes/wowcharacter.js
@@ -25,30 +25,6 @@ router.get('/:server/:charactername', function(req, res, next) {
   if (!fs.existsSync('./json/characters/' + server + '/' + firstLetter + '/' + character + '.json')) {
     fs.writeFileSync('./json/characters/' + server + '/' + firstLetter + '/' + character + '.json', '{"lastUpdated":[0],"character":[]}')
     console.log("it makes it to this point!")
-    fs.readFile('./json/characters/' + server + '/' + firstLetter + '/' + character + '.json', 'utf-8', function(err, data) {
-      if (err) throw err
-      //copied from here
-      var arrayOfObjects = JSON.parse(data)
-      lastJSONUpdate = arrayOfObjects.lastUpdated
-      console.log('Week has passed...calling API')
-        request('https://us.api.battle.net/wow/character/' + req.params.server + '/' + req.params.charactername + '?fields=audit,titles,talents,stats,statistics,reputation,quests,pvp,progression,professions,petSlots,mounts,pets,feed,items,achievements,appearance,guild,hunterPets&locale=en_US&apikey=' + process.env.BATTLENET_API_KEY,
-          function(error, response, body) {
-            fs.readFile('./json/characters/' + server + '/' + firstLetter + '/' + character + '.json', 'utf-8', function(err, data) {
-              var arrayOfObjects = JSON.parse(data)
-              arrayOfObjects.lastUpdated = [];
-              arrayOfObjects.lastUpdated.push(Date.now())
-              jsonconvert = JSON.parse(body)
-              arrayOfObjects.character = [];
-              arrayOfObjects.character.push(jsonconvert);
-              console.log('arrayOfObjects ', arrayOfObjects)
-              fs.writeFile('./json/characters/' + server + '/' + firstLetter + '/' + character + '.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
-                if (err) throw err
-                console.log('JSON Sent!');
-                sendJSON();
-              })
-            })
-          });
-    });
   }
 
 
