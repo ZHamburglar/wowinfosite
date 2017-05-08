2970dbb08d19a60473e53f3384d9610b3b56c502
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-07 19:45:10 -0500

    items now update the json

--- backend/routes/wowitem.js
+++ backend/routes/wowitem.js
@@ -25,36 +25,55 @@ router.get('/item/:itemid', function(req, res, next) {
   var itemlength = itemArea.length
   var itemIndex = itemArea.slice(itemlength -3, itemlength)
   var folderIndex = itemArea.slice(0, itemlength -3)
-  console.log("Item area: ", itemIndex);
-  console.log("item folder: ", folderIndex);
+  // console.log("Item area: ", itemIndex);
+  // console.log("item folder: ", folderIndex);
   if (folderIndex < 1000){
     let folderIndex = 1
   }
   if (!fs.existsSync('./json/items/' + folderIndex)){
       fs.mkdirSync('./json/items/' + folderIndex);
   }
-  // if (!fs.existsSync('./json/characters/' + server + '/' + firstLetter)){
-  //     fs.mkdirSync('./json/characters/' + server + '/' + firstLetter);
-  // }
   if (!fs.existsSync('./json/items/' + folderIndex + '/'+ itemArea + '.json')) {
     fs.writeFileSync('./json/items/' + folderIndex + '/'+ itemArea + '.json', '{"lastUpdated":[0],"item":[]}')
-    console.log("it makes it to this point!")
+    console.log(gutil.colors.green("it makes it to this point!"));
   }
+      fs.readFile('./json/items/' + folderIndex + '/'+ itemArea + '.json', 'utf-8', function(err, data) {
+        if (err) throw err
+        var arrayOfObjects = JSON.parse(data)
+        lastJSONUpdate = arrayOfObjects.lastUpdated
+        if ((Date.now() - lastJSONUpdate) > 1400000000) {
+          request('https://us.api.battle.net/wow/item/'+req.params.itemid+'?locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
+            function(error, response, body) {
+              if(error) console.log("THERE WAS AN ERROR ", error)
+              fs.readFile('./json/items/' + folderIndex + '/'+ itemArea + '.json', 'utf-8', function(err, data) {
+                var arrayOfObjects = JSON.parse(data)
+                arrayOfObjects.lastUpdated = [];
+                arrayOfObjects.lastUpdated.push(Date.now())
+                jsonconvert = JSON.parse(body)
+                arrayOfObjects.character = [];
+                arrayOfObjects.character.push(jsonconvert);
+                // console.log('arrayOfObjects ', arrayOfObjects)
+                fs.writeFile('./json/items/' + folderIndex + '/'+ itemArea + '.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+                  if (err) throw err
+                  console.log('JSON Sent!');
+                  sendJSON();
+                })
+              })
+              console.log('error:', error);
+              console.log('statusCode:', response && response.statusCode);
+            });
+        } else {
+          console.log('sanity check else')
+          sendJSON();
+        }
+      });
 
-
-
-
-
-
-
-
-  request('https://us.api.battle.net/wow/item/'+req.params.itemid+'?locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
-    function (error, response, body) {
-      console.log('error:', error);
-      console.log('statusCode:', response && response.statusCode);
-      console.log('body:', body);
-      res.send(body);
-  });
+    function sendJSON() {
+      var filePath = './json/items/' + folderIndex + '/'+ itemArea + '.json'
+      var resolvedPath = path.resolve(filePath);
+      console.log("JSON Being sent: ", resolvedPath);
+      return res.sendFile(resolvedPath);
+    }
 });
 
 router.get('/set/:setid', function(req, res, next) {
