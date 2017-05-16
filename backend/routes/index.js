c6d41d2c0d7db98c3a4a24041e30e0170e53d903
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-15 21:32:40 -0500

    random gif now works

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,19 +6,19 @@ router.get('/', function(req, res, next) {
   var gifsource= Math.floor(Math.random() * (4)) + 1;
   console.log('gifsource: ', gifsource)
   if (gifsource == 1){
-    var gif ="deathwing.webm"
+    var gif ="/animatedimages/deathwing.webm"
   } else if (gifsource == 2){
     console.log(2)
-    var gif ="grommash.webm"
+    var gif ="/animatedimages/grommash.webm"
   } else if (gifsource == 3){
     console.log(3)
-    var gif ="lichking.webm"
+    var gif ="/animatedimages/lichking.webm"
   } else if (gifsource == 4){
     console.log(4)
-    var gif ="pandaria.webm"
+    var gif ="/animatedimages/pandaria.webm"
   }
 
-  res.render('index', { title: 'WoW Node Backend' });
+  res.render('index', { title: 'WoW Node Backend', gif: gif });
 });
 
 module.exports = router;
