6e905b56a67ea7f7f6d0491bf820b58128ee686c
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-15 21:09:46 -0500

    working on dynamic background

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,7 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var gifsource= Math.floor(Math.random() * (4)) + 1;
+  console.log('gifsource: ', gifsource)
+  if (gifsource == 1){
+    var gif ="deathwing.webm"
+  } else if (gifsource == 2){
+    console.log(2)
+    var gif ="grommash.webm"
+  } else if (gifsource == 3){
+    console.log(3)
+    var gif ="lichking.webm"
+  } else if (gifsource == 4){
+    console.log(4)
+    var gif ="pandaria.webm"
+  }
+
+  res.render('index', { title: 'WoW Node Backend' });
 });
 
 module.exports = router;
