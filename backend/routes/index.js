8e49b57d95fc91ad286c5ec026c0244a350dff8f
Kollin Brandenburg <kollinbrandenburg@gmail.com>
2017-05-19 13:35:27 -0500

    email addon

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const nodemailer = require('nodemailer');
+require('dotenv').config();
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -17,8 +21,46 @@ router.get('/', function(req, res, next) {
     console.log(4)
     var gif ="/animatedimages/pandaria.webm"
   }
+  var y = new Date();
+  var year = y.getFullYear();
 
-  res.render('index', { title: 'WoW Node Backend', gif: gif });
+  res.render('index', { title: 'WoW Node Backend', gif: gif, year: year });
 });
+
+
+router.get('/mail', function(req, res, next) {
+'use strict';
+var date = new Date();
+var day = date.getDate();
+var month = date.getMonth()+1; //January is 0
+var year = date.getFullYear();
+
+// create reusable transporter object using the default SMTP transport
+let transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: process.env.EMAIL_ADDRESS,
+        pass: process.env.EMAIL_PASS
+    }
+});
+
+// setup email data with unicode symbols
+let mailOptions = {
+    from: '"Azeroth Backend" <'+process.env.EMAIL_ADDRESS+'>', // sender address
+    to: 'kollinbrandenburg@gmail.com', // list of receivers
+    subject: 'Azeroth Unlimited Weekly Update '+month+'/'+day+'/'+year, // Subject line
+    text: 'Hello world ? My bad', // plain text body
+    html: '<b>Hello world ? hahahhahahah Test 2 this is the body</b>' // html body
+};
+
+// send mail with defined transport object
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        return console.log(error);
+    }
+    console.log('Message %s sent: %s', info.messageId, info.response);
+});
+});
+
 
 module.exports = router;
--- backend/routes/wowguild.js
+++ backend/routes/wowguild.js
@@ -12,14 +12,60 @@ require('dotenv').config()
 router.get('/:server/:guildname', function(req, res, next) {
   splitFolderBuild("guilds", req.params.server, req.params.guildname);
 
-  request('https://us.api.battle.net/wow/guild/'+req.params.server+'/'+req.params.guildname+'?fields=news,achievements,members,challenge&locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
-    function (error, response, body) {
-      console.log('error:', error);
-      console.log('statusCode:', response && response.statusCode);
-      console.log('body:', body);
-      res.send(body);
+    var server = req.params.server
+    var guildname = req.params.guildname
+    var firstLetter = character.charAt(0).toUpperCase()
+    console.log("First Letter ", firstLetter)
+      fs.readFile('./json/guilds/' + server + '/' + firstLetter + '/' + guildname + '.json', 'utf-8', function(err, data) {
+        if (err) throw err
+        var arrayOfObjects = JSON.parse(data)
+        lastJSONUpdate = arrayOfObjects.lastUpdated
+        // if ((Date.now() - lastJSONUpdate) > 100000000) {
+
+        if ((Date.now() - lastJSONUpdate) > 1000) {
+          request('https://us.api.battle.net/wow/guild/'+req.params.server+'/'+req.params.guildname+'?fields=news,achievements,members,challenge&locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
+            function(error, response, body) {
+              if(error) console.log("THERE WAS AN ERROR ", error)
+              fs.readFile('./json/guilds/' + server + '/' + firstLetter + '/' + guildname + '.json', 'utf-8', 'utf-8', function(err, data) {
+                var arrayOfObjects = JSON.parse(data)
+                arrayOfObjects.lastUpdated = [];
+                arrayOfObjects.lastUpdated.push(Date.now())
+                jsonconvert = JSON.parse(body)
+                arrayOfObjects.guilds = [];
+                arrayOfObjects.guilds.push(jsonconvert);
+                arrayOfObjects.history.push({
+                  "timestamp":jsonconvert.lastModified
+                });
+                fs.writeFile('./json/characters/' + server + '/' + firstLetter + '/' + character + '.json', JSON.stringify(arrayOfObjects), 'utf-8', function(err) {
+                  if (err) throw err
+                  console.log('JSON Sent!');
+                  sendJSON();
+                })
+              })
+              console.log('error:', error);
+              console.log('statusCode:', response && response.statusCode);
+            });
+        } else {
+          sendJSON();
+        }
+      });
+    function sendJSON() {
+      var filePath = './json/characters/' + server + '/' + firstLetter + '/' + character + '.json'
+      var resolvedPath = path.resolve(filePath);
+      console.log("JSON Being sent: ", resolvedPath);
+      return res.sendFile(resolvedPath);
+    }
   });
-});
+
+//   request('https://us.api.battle.net/wow/guild/'+req.params.server+'/'+req.params.guildname+'?fields=news,achievements,members,challenge&locale=en_US&apikey='+process.env.BATTLENET_API_KEY,
+//     function (error, response, body) {
+//       console.log('error:', error);
+//       console.log('statusCode:', response && response.statusCode);
+//       console.log('body:', body);
+//       res.send(body);
+//
+//   });
+// });
 
 
 module.exports = router;
